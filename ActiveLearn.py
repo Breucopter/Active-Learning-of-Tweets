222ba1526c178486ee6e1fa6b56b3d4539108d27
Justin Breucop <justin.breucop@datasift.com>
2014-12-02 10:56:35 +0000

    Modified ActiveLearn & fixed git issues...

--- ActiveLearn.py
+++ ActiveLearn.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 import json
+import sys
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.cross_validation import KFold
 
 def QuickConvert(iterable):
 
@@ -11,29 +14,67 @@ def QuickConvert(iterable):
 		json_object = json.loads(line)
 		try:
 			new_data = pd.DataFrame({'content':json_object['interaction']['content'],
-									'tag': json_object['interaction']['tags']})
+									'tag': json_object['interaction']['label']},index=[json_object['interaction']['id']])
 			frame = frame.append(new_data)
 		except:
 			continue
 	return frame
 
-with open('./train-fossil.json') as f:
+# def ReturnMins(result_list,n):
+# '''Return the n lowest decided labels'''
+# 	decided_prob = [max(result_list[x,:]) for x in xrange(0,result_list.shape[0])]
+# 	mins = []
+# 	for i,x in enumerate(decided_prob):
+# 		if i < n:
+# 			mins.append(min(decided_prob))
+# 			decided_prob.remove(min(decided_prob))
+# 		else:
+# 			continue
+# 	#generate location of the minimums and return the list
+# 	decided_prob = [max(result_list[x,:]) for x in xrange(0,result_list.shape[0])]
+# 	min_index = [decided_prob.index(x) for x in mins]
+
+# filename=sys.argv[1]
+with open('./fixtures/airlines.json') as f:
 	json_raw = f.readlines()
 
 data = QuickConvert(json_raw)
+size = len(data.index)
+
+seed = np.random.choice(size,size=50,replace=False)
 
 tfidf_transformer = TfidfVectorizer()
-X_train_tfidf = tfidf_transformer.fit_transform(data['content'])
+global_tfidf = tfidf_transformer.fit_transform(data['content'])
 
-with open('./test-fossil.json') as f:
-	json_raw_test = f.readlines()
-test_data = QuickConvert(json_raw_test)
+clf = MultinomialNB().fit(global_tfidf[seed], data.iloc[seed]['tag'])
+results = clf.predict_proba(global_tfidf)
 
-clf = MultinomialNB().fit(X_train_tfidf, data['tag'])
 
-X_new_tfidf = tfidf_transformer.transform(test_data["content"])
+#Return the 50 lowest decided labels
+decided_prob = [max(results[x,:]) for x in xrange(0,results.shape[0])]
+mins = []
+for i,x in enumerate(decided_prob):
+	if i < 50:
+		mins.append(min(decided_prob))
+		decided_prob.remove(min(decided_prob))
+	else:
+		continue
 
-predicted = clf.predict(X_new_tfidf)
+for x in mins:
+	print x
+#generate location of the minimums and return the list
+decided_prob = [max(results[x,:]) for x in xrange(0,results.shape[0])]
+min_index = [decided_prob.index(x) for x in mins]
+print "Score on mins:",clf.score(global_tfidf[min_index],data.iloc[min_index]['tag']),"Global Score:",clf.score(global_tfidf,data['tag'])
+data['Predicted'] = clf.predict(global_tfidf)
+print data.loc[min_index]
 
-for doc, category in zip(test_data["content"],predicted):
-    print('%r => %s' % (doc,category))
+
+
+# X_new_tfidf = tfidf_transformer.transform(test_data["content"])
+
+# predicted = clf.predict(X_new_tfidf)
+
+# for doc, category in zip(data["content"],predicted):
+# 	print('%r => %s' % (doc,category))
+
